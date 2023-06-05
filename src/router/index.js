dd5c06dc41db1f8970ae81a8b5cb4e7e32deff18
11118834910852823 <2686178171@qq.com>
2023-06-05 20:24:17 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,75 +1,75 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import CinemaList from '@/views/CinemaList.vue'
-import FilmList from '@/views/FilmList.vue'
-import FilmTab from '@/views/FilmTab.vue'
-import FilmInfo from '@/views/FilmInfo.vue'
-import SearchCinema from '@/views/SearchCinema.vue'
-import CityList from '@/views/CityList.vue'
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import CinemaList from "@/views/CinemaList.vue";
+import FilmList from "@/views/FilmList.vue";
+import FilmTab from "@/views/FilmTab.vue";
+import FilmInfo from "@/views/FilmInfo.vue";
+import SearchCinema from "@/views/SearchCinema.vue";
+import CityList from "@/views/CityList.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: '/films'
+    path: "/",
+    redirect: "/films",
   },
   {
-    path: '/films',
+    path: "/films",
     component: FilmTab,
-    redirect: '/films/nowPlaying',
+    redirect: "/films/nowPlaying",
     /* filmType为电影类型，active为激活的tab所有 */
     children: [
       {
-        path: 'nowPlaying',
+        path: "nowPlaying",
         component: FilmList,
-        name: 'nowPlaying',
+        name: "nowPlaying",
         meta: {
           tabbarShow: true,
           filmType: 1,
-          active: 0
-        }
+          active: 0,
+        },
       },
       {
-        path: 'commingSoon',
+        path: "commingSoon",
         component: FilmList,
-        name: 'commingSoon',
+        name: "commingSoon",
         meta: {
           tabbarShow: true,
           filmType: 2,
-          active: 1
-        }
-      }
-    ]
+          active: 1,
+        },
+      },
+    ],
   },
   {
-    path: '/films/:id',
+    path: "/films/:id",
     component: FilmInfo,
     props: true,
     meta: {
-      tabbarShow: false
-    }
+      tabbarShow: false,
+    },
   },
   {
-    path: '/cinemas',
+    path: "/cinemas",
     component: CinemaList,
     meta: {
-      tabbarShow: true
-    }
+      tabbarShow: true,
+    },
   },
   {
-    path: '/cinemas/search',
-    component: SearchCinema
+    path: "/cinemas/search",
+    component: SearchCinema,
   },
   {
-    path: '/city',
-    component: CityList
-  }
-]
+    path: "/city",
+    component: CityList,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "hash",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
